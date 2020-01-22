5ea975321deddfc708c420e3f45e617cfa8d5311
Roelof Rietbroek (@grace) <roelof@geod.uni-bonn.de>
2020-01-22 16:27:57 +0100

    make better time formatting (quaterly grids and half yearly time tags)

--- plotGannt.py
+++ plotGannt.py
@@ -11,7 +11,7 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as font_manager
 import matplotlib.dates
-from matplotlib.dates import WEEKLY,MONTHLY, DateFormatter, rrulewrapper, RRuleLocator, datestr2num
+from matplotlib.dates import num2date,date2num,YEARLY,WEEKLY,MONTHLY, DateFormatter, rrulewrapper, RRuleLocator, datestr2num, MonthLocator,YearLocator
 import numpy as np
 import collections
 from yaml import load as yamlload
@@ -50,27 +50,36 @@ def plotGantt(args):
     ylabels=[]
     pos=[]
     myFmt = DateFormatter('%b %Y')
+    # myFmt = DateFormatter('%Y')
     if args.locale:
         locale.setlocale(locale.LC_ALL,args.locale)
    
     ax = fig.add_subplot(111)
     yval=len(data)
+    axlim=[sys.float_info.max,sys.float_info.min]
     for task in data:
         for st,nd in task.slots:
             ax.barh(yval,width=nd-st,height=0.9,left=st,color=task.color)
+            axlim[0]=min(axlim[0],st)
+            axlim[1]=max(axlim[1],nd)
         ylabels.append(task.name)
         pos.append(yval)
         yval-=1
-                    
+    ax.set_xlim(axlim)
     ax.xaxis.set_major_formatter(myFmt)
-    rule = rrulewrapper(MONTHLY, interval=3)
-    loc = RRuleLocator(rule)
-    ax.xaxis.set_major_locator(loc)
+    #rule = rrulewrapper(MONTHLY, interval=3)
+    # rule = rrulewrapper(YEARLY, interval=1)
+    # loc = RRuleLocator(rule)
+    hyrloc= MonthLocator((1,7))
+    qloc= MonthLocator((1,4,7,10))
+    ax.xaxis.set_major_locator(hyrloc)
+    ax.xaxis.set_minor_locator(qloc)
     labelsx = ax.get_xticklabels()
     plt.setp(labelsx, rotation=30, fontsize=fsize)
     locsy, labelsy = plt.yticks(pos,ylabels)
     plt.setp(labelsy, fontsize = fsize)
-    ax.grid(color = 'grey', linestyle = ':')
+    ax.grid(color = 'grey',which='major', linestyle = '-')
+    ax.grid(color = 'grey', which='minor',linestyle = ':')
 
     plt.tight_layout()
 
@@ -93,6 +102,12 @@ def main(argv):# plot
     
     parser.add_argument('-r','--ratio', metavar='ASPECTRATIO', type=float,
             help='Specify the aspect ratio (w/h as a float)  of the plot, defaults to 16:9',default=16/9)
+    
+    # parser.add_argument('-q','--quarterly', default=False,action='store_true',
+            # help='Make a plot which has esily recognizable quaterlies')
+
+    # parser.add_argument('
+    
     args=parser.parse_args(argv[1:]) 
     
     plotGantt(args)
